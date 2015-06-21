50643202b37a114e07c944b140ca9bf1fc9f5c6e
sc14ga <sc14ga@leeds.ac.uk>
2015-06-21 21:23:07 +0100

    Phone Call Test
    Test module that programmatically makes a phone call.

---
+++ Tests/CallTest/src/com/example/calltest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.calltest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Button call = (Button) findViewById(R.id.button1);	
+		call.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+		    	Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+447518924080"));
+		    	startActivity(intent);
+		    }
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
