982072105bdd4f64fcc9ae842bd2afa42e4d9221
sc14ga <sc14ga@leeds.ac.uk>
2015-07-25 21:58:45 +0100

    Fake Call Test
    Trying to imitate the incoming call screen, using the existing classes
    and layouts from the Android source code.

--- Tests/FakeCallTest/gen/com/example/fakecalltest/R.java
+++ Tests/FakeCallTest/gen/com/example/fakecalltest/R.java
@@ -10,6 +10,15 @@ package com.example.fakecalltest;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int incall_call_banner_background=0x7f040001;
+        /**  semitransparent black 
+         */
+        public static final int incall_call_banner_text_color=0x7f040002;
+        /**  Put on top of each photo, implying 80% darker than usual. 
+         */
+        public static final int on_hold_dim_effect=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,52 +27,59 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        /**  Height of the "call banner" overlay on top of the upper part of
+         the call info area. 
+         */
+        public static final int call_banner_height=0x7f050002;
+        /**  Right padding for name and number fields in the call banner.
+         This padding is used to ensure that ultra-long names or
+         numbers won't overlap the elapsed time indication. 
+         */
+        public static final int call_banner_name_number_right_padding=0x7f050005;
+        /**  Padding at the left and right edges of the "call banner". 
+         */
+        public static final int call_banner_side_padding=0x7f050003;
+        /**  Padding at the top and bottom edges of the "call banner". 
+         */
+        public static final int call_banner_top_bottom_padding=0x7f050004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
-        public static final int elapsedTime=0x7f080005;
-        public static final int label=0x7f08000f;
-        public static final int lowerTitle=0x7f080004;
-        public static final int lowerTitleIcon=0x7f080003;
-        public static final int lowerTitleViewGroup=0x7f080002;
-        public static final int mainCallCard=0x7f080000;
-        public static final int menuButtonHint=0x7f08000c;
-        public static final int name=0x7f08000e;
-        public static final int otherCallOnHoldInfoArea=0x7f080009;
-        public static final int otherCallOnHoldName=0x7f08000a;
-        public static final int otherCallOnHoldStatus=0x7f08000b;
-        public static final int otherCallOngoingInfoArea=0x7f080006;
-        public static final int otherCallOngoingName=0x7f080007;
-        public static final int otherCallOngoingStatus=0x7f080008;
-        public static final int phoneNumber=0x7f080010;
-        public static final int photo=0x7f08000d;
-        public static final int upperTitle=0x7f080001;
+        public static final int action_settings=0x7f090009;
+        public static final int callTypeLabel=0x7f090008;
+        public static final int dim_effect_for_primary_photo=0x7f090001;
+        public static final int elapsedTime=0x7f090007;
+        public static final int label=0x7f090006;
+        public static final int labelAndNumber=0x7f090004;
+        public static final int name=0x7f090003;
+        public static final int phoneNumber=0x7f090005;
+        public static final int photo=0x7f090000;
+        public static final int primary_call_banner=0x7f090002;
     }
     public static final class layout {
         public static final int call_card=0x7f030000;
-        public static final int call_card_person_info=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int contactPhoto=0x7f060006;
         /**  Used in FakePhoneActivity test code.  DO NOT TRANSLATE. 
          */
-        public static final int fake_phone_activity_infoText_text=0x7f050004;
+        public static final int fake_phone_activity_infoText_text=0x7f060004;
         /**  Used in FakePhoneActivity test code.  DO NOT TRANSLATE. 
          */
-        public static final int fake_phone_activity_phoneNumber_text=0x7f050003;
+        public static final int fake_phone_activity_phoneNumber_text=0x7f060003;
         /**  Used in FakePhoneActivity test code.  DO NOT TRANSLATE. 
          */
-        public static final int fake_phone_activity_placeCall_text=0x7f050005;
-        public static final int hello_world=0x7f050001;
+        public static final int fake_phone_activity_placeCall_text=0x7f060005;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -87,10 +103,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int PrimaryCallInfoPrimaryCallBanner=0x7f070002;
     }
 }
--- Tests/FakeCallTest/src/com/example/fakecalltest/MainActivity.java
+++ Tests/FakeCallTest/src/com/example/fakecalltest/MainActivity.java
@@ -1,29 +1,50 @@
 package com.example.fakecalltest;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private ImageView mPhoto;
+    private View mPhotoDimEffect;
+    
+	private TextView mName;
+    private TextView mPhoneNumber;
+    private TextView mLabel;
+    private TextView mCallTypeLabel;
+	
+	private TextView mElapsedTime;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.call_card);
 		
-		TextView upperTitle = (TextView) findViewById(R.id.upperTitle);
-		TextView lowerTitle = (TextView) findViewById(R.id.lowerTitle);
-	
-		Intent inCallIntent = new Intent();
-		inCallIntent.setClassName("com.android.phone", "com.android.phone.InCallScreen");
-		startActivity(inCallIntent);
+		//mPhoto = (ImageView) findViewById(R.id.photo);
+        mPhotoDimEffect = findViewById(R.id.dim_effect_for_primary_photo);
+
+        mName = (TextView) findViewById(R.id.name);
+        mPhoneNumber = (TextView) findViewById(R.id.phoneNumber);
+        mLabel = (TextView) findViewById(R.id.label);
+        mCallTypeLabel = (TextView) findViewById(R.id.callTypeLabel);
+        
+        mElapsedTime = (TextView) findViewById(R.id.elapsedTime);
+        
+        //showImage(mPhoto, R.drawable.picture_unknown);
+        
+        mName.setText("George");
+        mPhoneNumber.setText("07518924080");
+        mElapsedTime.setText("00:02");
+        mLabel.setText("Label");
+        mCallTypeLabel.setText("Call");
+        
 	}
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// call ActionBarDrawerToggle.onOptionsItemSelected(), if it returns true
-        // then it has handled the app icon touch event
-	    return super.onOptionsItemSelected(item);
-	}
 }
