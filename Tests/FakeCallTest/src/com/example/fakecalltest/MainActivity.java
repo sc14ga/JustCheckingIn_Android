954858b3e2f309f5360434aac670c04be5dd6d2b
sc14ga <sc14ga@leeds.ac.uk>
2015-07-28 22:23:33 +0100

    Fake Call Test
    Added vibration and ringtone when the incoming call screen appears.

--- Tests/FakeCallTest/src/com/example/fakecalltest/MainActivity.java
+++ Tests/FakeCallTest/src/com/example/fakecalltest/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.fakecalltest;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	private ImageView mPhoto;
-    private View mPhotoDimEffect;
+	private Button accept;
+	private Button decline;
     
 	private TextView mName;
     private TextView mPhoneNumber;
@@ -24,14 +35,16 @@ public class MainActivity extends Activity {
 	
 	private TextView mElapsedTime;
 	
+	private Vibrator v;
+	long pattern[]={0,800,200,1200,300,2000,400};
+	
+	private MediaPlayer mMediaPlayer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.call_card);
-		
-		//mPhoto = (ImageView) findViewById(R.id.photo);
-        mPhotoDimEffect = findViewById(R.id.dim_effect_for_primary_photo);
 
         mName = (TextView) findViewById(R.id.name);
         mPhoneNumber = (TextView) findViewById(R.id.phoneNumber);
@@ -49,6 +62,64 @@ public class MainActivity extends Activity {
         mLabel.setText("Label");
         mCallTypeLabel.setText("Call");
         
+        Uri ringtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);		
+		playSound(this, ringtone);
+		
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        
+        boolean hasVibrator = v.hasVibrator();
+        Toast.makeText(getApplicationContext(), String.valueOf(hasVibrator), Toast.LENGTH_LONG).show();
+        v.vibrate(pattern,1);
+        
+        accept = (Button) findViewById(R.id.acceptButton);
+        accept.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				v.cancel();
+				stopMediaPlayer();
+				//Intent intent
+			}
+		});
+        decline = (Button) findViewById(R.id.declineButton);
+        decline.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				v.cancel();
+				stopMediaPlayer();
+				finish();
+			}
+		});
+        
+
+        
+        // pass the number of millseconds fro which you want to vibrate the phone here we
+        // have passed 2000 so phone will vibrate for 2 seconds.
+
+         //v.vibrate(2000);
+
+	     // If you want to vibrate  in a pattern
+	     
+	     // 2nd argument is for repetition pass -1 if you do not want to repeat the Vibrate
+	     
+        
 	}
 	
+	private void playSound(Context context, Uri alert) {
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(context, alert);
+            final AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            if (audioManager.getStreamVolume(AudioManager.STREAM_RING) != 0) {
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_RING);
+                mMediaPlayer.prepare();
+                mMediaPlayer.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	public void stopMediaPlayer(){
+    	mMediaPlayer.stop();
+    }
 }
